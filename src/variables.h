04b92bdaa3d0108bf85f68a0af2bbd30b054ab8b
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-22 13:09:50 +0200

    Update variables.h

--- src/variables.h
+++ src/variables.h
@@ -1,5 +1,8 @@
 #define $DEFAULT_AMMO 25
 #define $DEFAULT_AIM_LOCK 1
+#define $DEFAULT_AIM_X_POSITION 10;
+#define $DEFAULT_AIM_Y_POSITION 0;
+#define $DEFAULT_AIM_WHEEL 0;
 
 int weapon_ammo = $DEFAULT_AMMO;
 bool aim_lock = $DEFAULT_AIM_LOCK;
