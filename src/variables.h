b9f93f85194d485ad3bd2340ceb197812f7f3e42
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-22 12:44:45 +0200

    Update variables.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/variables.h
+++ src/variables.h
@@ -1,7 +1,5 @@
-#define $APP_NAME "Advanced Game Control"
-#define $APP_VERSION "v0.1.7"
-
 #define $DEFAULT_AMMO 25
 #define $DEFAULT_AIM_LOCK 10
 
 int weapon_ammo = $DEFAULT_AMMO;
+int aim_lock = $DEFAULT_AIM_LOCK;
