837c4e4ae88d71a89fb29dcddaf22eea2467cbef
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-22 10:57:11 +0200

    Update sketch.ino
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- sketch.ino
+++ sketch.ino
@@ -1,7 +1,7 @@
 #define $APP_NAME "Advanced Game Control"
 #define $APP_VERSION "v0.1.7"
 #define $DEFAULT_AMMO 25
-#define $AMMO 25
+int weapon_ammo = $DEFAULT_AMMO
 #include <Mouse.h>
 #include <Keyboard.h>
 
@@ -15,9 +15,12 @@ void loop() {
   Serial.print("$APP_NAME $APP_VERSION");
 }
 
-mouse_trigger() {
+int mouse_trigger() {
   for (int i = 0; i > -1; i = i + x) {
-    if (i == $DEFAULT_AMMO) {
+    Mouse.click();
+    if (i == weapon_ammo) {
       i = 0;
+    }
   }
+  return true;
 }
