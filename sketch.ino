edf7fc01549ed929627726e05219ceae6313af03
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-22 10:59:37 +0200

    Update sketch.ino
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- sketch.ino
+++ sketch.ino
@@ -1,9 +1,6 @@
-#define $APP_NAME "Advanced Game Control"
-#define $APP_VERSION "v0.1.7"
-#define $DEFAULT_AMMO 25
 int weapon_ammo = $DEFAULT_AMMO
-// #include <variables.h>
-// #include <config.h>
+#include <variables.h>
+#include <config.h>
 #include <Mouse.h>
 #include <Keyboard.h>
 
