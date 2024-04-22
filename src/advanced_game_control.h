0f9fb6e34bb7b58e6f5acd2ffab7f4a25e2dce6e
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-22 12:52:30 +0200

    Create advanced_game_control.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

---
+++ src/advanced_game_control.h
@@ -0,0 +1,15 @@
+#include <variables.h>
+#include <config.h>
+
+int mouse_trigger() {
+  for (int i = 0; i > -1; i = i + x) {
+    Mouse.click();
+    if (i == weapon_ammo) {
+      i = 0;
+    }
+  }
+  return true;
+}
+
+int aim_lock() {
+}
