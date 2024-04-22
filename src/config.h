0489ae715dfce31518a4c5d6e518f40622bc6d88
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-22 13:15:21 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -5,5 +5,11 @@ int set(String id, String value) {
   switch (config_) {
     case 0:
       weapon_ammo = str(int(value));
+    case 1:
+      aim_x = str(int(value));
+    case 2:
+      aim_y = str(int(value));
+    case 3:
+      aim_wheel = str(int(value));
   }
 }
