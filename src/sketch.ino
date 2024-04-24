79e7fbd483bc5cf3023b012b33ec47255d1328d1
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-24 18:20:36 +0200

    Update sketch.ino
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/sketch.ino
+++ src/sketch.ino
@@ -9,7 +9,12 @@ void setup() {
   Mouse.begin();
   Keyboard.begin();
   Serial.begin(23000);
-  Serial.print("NanoAim 0.0.9");
+  Serial.println("NanoAim 0.0.9");
+  while (Serial.available() == 0) { 
+  }
+  Serial.println("> startup_delay = 30.000");
+  delay(30000);
+  String input = Serial.parseString();
 }
 
 void loop() {
