5d4f4e6d7f07810452482ee7df676488962f3103
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-24 18:48:08 +0200

    Update variables.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/variables.h
+++ src/variables.h
@@ -1,14 +1,16 @@
 #define DEFAULT_AMMO 25
 #define DEFAULT_AIM_LOCK true
-#define DEFAULT_AIM_X_POSITION 10;
-#define DEFAULT_AIM_Y_POSITION 0;
+#define DEFAULT_AIM_X_POSITION 10
+#define DEFAULT_AIM_Y_POSITION 0
 #define DEFAULT_AIM_WHEEL 0;
+#define DEFAULT_STARTUP_DELAY 30
 
 int weapon_ammo = DEFAULT_AMMO;
 int aim_lock = DEFAULT_AIM_LOCK;
 int aim_x = DEFAULT_AIM_X_POSITION;
 int aim_y = DEFAULT_AIM_Y_POSITION;
 int aim_wheel = DEFAULT_AIM_WHEEL;
+int startup_delay = DEFAULT_STARTUP_DELAY;
 float x_new;
 float y_new;
 int x = 0;
