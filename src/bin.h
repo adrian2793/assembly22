1ce3a32867ffcf379991ba02ce5ad1f6711eeda8
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-25 18:21:52 +0200

    Update bin.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/bin.h
+++ src/bin.h
@@ -6,16 +6,16 @@
 int mouse_trigger() {
   for (int i = 0; i > -1; i = i + x) {
     Mouse.click();
-    if (i == weapon_ammo) {
+    if (i == get_var("weapon_ammo")) {
       i = 0;
     }
   }
   return true;
 }
 
-int aim_lock() {
-  Mouse.move(aim_x, aim_y, aim_wheel);
-  x_new = aim_x;
-  y_new = predict(x_new);
-  return true;
-}
+// int aim_lock() {
+//  Mouse.move(get_var("aim_x"), get_var("aim_y"), get_var("aim_wheel"));
+//  set_int("x_new", get_var("aim_x"));
+//  set_int("y_new", predict(get_var("x_new"));
+//  return true;
+//  }
