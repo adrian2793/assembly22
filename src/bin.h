8bffb75e60579d3e588a3f0cc3e2ccec40a520ca
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-26 08:19:04 +0200

    Update bin.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/bin.h
+++ src/bin.h
@@ -3,6 +3,28 @@
 #include "variables.h"
 // #include "predictions.h"
 
+ifndef variables_h
+#define variables_h
+
+extern int weapon_ammo;
+extern bool aim_lock;
+extern bool mouse_trigger_bool;
+extern int aim_x;
+extern int aim_y;
+extern int aim_wheel;
+extern int startup_delay;
+extern String string_input;
+extern int int_input;
+extern int x;
+extern float x_new;
+extern float y_new;
+
+bool set_int(int id1, int value1);
+bool set_bool(int id2, bool value2);
+bool set_string(int id3, String value3);
+
+#endif
+
 bool mouse_trigger() {
   for (int i = 0; i > -1; i = i + x) {
     Mouse.click();
