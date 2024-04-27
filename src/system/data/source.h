470d96e38290c45a43daedd745a816d945fa9357
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 12:40:25 +0200

    Update and rename recoil.h to source.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

---
+++ src/system/data/source.h
@@ -0,0 +1,10 @@
+// Source
+// * https://docs.google.com/spreadsheets/d/11tDzUNBq9zIX6_9Rel__fdAUezAQzSnh5AVYzCP060c/
+// * https://www.csweapons.com/
+
+// System
+
+#include "memory.h"
+
+weapon desert_eagle(85.2, 7, 188, 48.2, 18, 60);
+weapon r8_revolver(8.1, 8, 312, 20, 0, 40);
