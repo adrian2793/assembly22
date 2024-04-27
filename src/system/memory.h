26a277a6baa3dc7fcfa2aff6a544939153892053
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 12:31:31 +0200

    Update memory.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/memory.h
+++ src/system/memory.h
@@ -2,17 +2,15 @@
 
 class weapon {
   public:
-    int inaccuracy;
-    int ammunition;
-    int damage;
-    int recoil_amount;
-    int recoil_variance;
-    int recoil_angle_variance;
-    int smoothness = 0;
-    int get_smoothness();
+    weapon(int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0);
+    bool get_smoothness();
 }
 
-int object::get_smoothness() {
+weapon::weapon(int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0) {
+}
+
+bool weapon::get_smoothness() {
   this->smoothness = (recoil_amount - recoil_amount_variance) / 2;
   return this->smoothness;
 }
+
