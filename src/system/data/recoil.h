057544cd686df36538f4bcc566cb503c738981b6
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 11:10:23 +0200

    Update recoil.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/data/recoil.h
+++ src/system/data/recoil.h
@@ -1,3 +1,5 @@
 const int desert_eagle_inaccuracy = 85.2;
 const int desert_eagle_ammunition = 7;
 const int desert_eagle_damage = 294 / desert_eagle_ammunition;
+const int desert_eagle_recoil_amount = 48.2;
+const int desert_eagle_recoil_accuracy = 18;
