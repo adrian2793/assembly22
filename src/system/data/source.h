ac5437c84094e7bd984c49e819d1975036d0615f
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 16:45:58 +0200

    Update source.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/data/source.h
+++ src/system/data/source.h
@@ -6,6 +6,28 @@
 
 #include "memory.h"
 
+// Weapons
+
 weapon desert_eagle(6.2, 7, 42, 48.2, 18, 60);
 weapon r8_revolver(2.5, 8, 75, 20, 0, 40);
 weapon dual_berettas(9, 30, 17, 27, 4, 20);
+
+// Players
+
+// team = 0 -> Terrorists
+
+player player1(0, 0, 0);
+player player2(0, 0, 0);
+player player3(0, 0, 0);
+player player4(0, 0, 0);
+player player5(0, 0, 0);
+
+// team = 1 -> Counter Terrorists
+
+player player6(0, 0, 0, 1);
+player player7(0, 0, 0, 1);
+player player8(0, 0, 0, 1);
+player player9(0, 0, 0, 1);
+player player10(0, 0, 0, 1);
+
+// Maps
