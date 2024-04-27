502b731232fba9cf552e8819a4e5b84f6343a192
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 16:18:32 +0200

    Update memory.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/memory.h
+++ src/system/memory.h
@@ -2,10 +2,19 @@
 
 class weapon {
   public:
-    weapon(String name; int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0);
+    weapon(int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0);
 }
 
-weapon::weapon(String name; int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0) {
+weapon::weapon(int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0) {
   this->damage = this->damage / this->ammunition;
   this->smoothness = recoil_amount / recoil_angle_variance;
+}
+
+class player {
+  public:
+    int position_x; // Default -> 0
+    int position_y; // Default -> 0
+    int position_z; // Default -> 0
+    int team = 0; // 0 -> Counter Terrorists
+    bool visible = false;
 }
