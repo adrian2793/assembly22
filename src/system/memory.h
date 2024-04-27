c80ca00246eb19226cb5cd807eba177d130ea275
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 10:42:11 +0200

    Update memory.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/memory.h
+++ src/system/memory.h
@@ -1,2 +1,13 @@
-const bool recoil = true;
-const bool trigger_delay = true;
+// recoil
+
+const bool _0271 = true; // recoil
+const int _0271_smoothness = 1;
+
+// trigger
+
+const bool _0272 = true; // trigger
+const bool _0272_delay = true;
+
+// aim
+
+const bool _0273 = true; // aim
