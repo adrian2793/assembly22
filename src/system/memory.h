f7fa9a221470394a456da28f30996987c7405122
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 12:03:06 +0200

    Update memory.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/memory.h
+++ src/system/memory.h
@@ -6,10 +6,17 @@ include "recoil.h"
 
 class object {
   public:
-    int variance;
-    int angle_variance;
-    bool smoothness(
+    const int inaccuracy;
+    const int ammunition;
+    const int damage;
+    const int recoil_amount;
+    const int recoil_variance;
+    const int recoil_angle_variance;
+    int smoothness;
+    int get_smoothness(
 }
 
-bool smoothness::smoothness(int) {
+int object::get_smoothness() {
+  this->smoothness = (recoil_amount - recoil_amount_variance) / 2;
+  return this->smoothness;
 }
