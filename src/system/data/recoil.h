1d27758f51db17f45d13cccb330e7deff61b50e2
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 11:28:30 +0200

    Update recoil.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/data/recoil.h
+++ src/system/data/recoil.h
@@ -4,14 +4,14 @@
 
 const int desert_eagle_inaccuracy = 85.2;
 const int desert_eagle_ammunition = 7;
-const int desert_eagle_damage = 294 / desert_eagle_ammunition;
+const int desert_eagle_damage = 188 / desert_eagle_ammunition;
 const int desert_eagle_recoil_amount = 48.2;
 const int desert_eagle_recoil_variance = 18;
 const int desert_eagle_recoil_angle_variance = 60;
 
 const int r8_revolver_inaccuracy = 8.1;
 const int r8_revolver_ammunition = 8;
-const int r8_revolver_damage = 600 / r8_revolver_ammunition;
+const int r8_revolver_damage = 312 / r8_revolver_ammunition;
 const int r8_revolver_recoil_amount = 20;
 const int r8_revolver_recoil_variance = 0;
 const int r8_revolver_recoil_angle_variance = 40;
