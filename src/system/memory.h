acc706e3babced13c225e5c24a69127ef8f6db35
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 12:36:54 +0200

    Update memory.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/memory.h
+++ src/system/memory.h
@@ -3,7 +3,6 @@
 class weapon {
   public:
     weapon(int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0);
-    bool get_smoothness();
 }
 
 weapon::weapon(int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0) {
