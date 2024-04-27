e5e65db42f7989425a5a6ec255424f8109644dbd
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 12:36:30 +0200

    Update memory.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/memory.h
+++ src/system/memory.h
@@ -7,10 +7,5 @@ class weapon {
 }
 
 weapon::weapon(int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0) {
+  this->smoothness = recoil_amount / recoil_angle_variance;
 }
-
-bool weapon::get_smoothness() {
-  this->smoothness = (recoil_amount - recoil_amount_variance) / 2;
-  return this->smoothness;
-}
-
