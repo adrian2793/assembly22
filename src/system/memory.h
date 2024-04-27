6237226eb94afe63f5faff73aa75140cc1b8b656
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-27 12:54:07 +0200

    Update memory.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/memory.h
+++ src/system/memory.h
@@ -2,9 +2,10 @@
 
 class weapon {
   public:
-    weapon(int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0);
+    weapon(String name; int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0);
 }
 
-weapon::weapon(int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0) {
+weapon::weapon(String name; int inaccuracy, int ammunition, int damage, int recoil_amount, int recoil_variance, int recoil_angle_variance, int smoothness = 0) {
+  this->damage = this->damage / this->ammunition;
   this->smoothness = recoil_amount / recoil_angle_variance;
 }
