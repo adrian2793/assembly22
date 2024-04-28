6c30f7be9b56aa48e8398b44604df1f98b501a90
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-28 11:03:48 +0200

    Update source.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/system/source.h
+++ src/system/source.h
@@ -8,7 +8,7 @@
 
 // Weapons
 
-weapon weapons[5] = {
+weapon weapon[5] = {
   weapon desert_eagle(6.2, 7, 42, 48.2, 18, 60);
   weapon r8_revolver(2.5, 8, 75, 20, 0, 40);
   weapon dual_berettas(9, 30, 17, 27, 4, 20);
