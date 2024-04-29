a2fff33ee29be967aa5f59170d4eeb278e3f3af7
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-04-29 13:22:33 +0200

    Update main.asm
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- main.asm
+++ main.asm
@@ -1,1 +1,15 @@
+section .text
+   global _start          ;must be declared for linker (gcc)
+	
+_start:                   ;tell linker entry point
+   mov	edx,1		  ;message length
+   mov	ecx,choice        ;message to write
+   mov	ebx,1		  ;file descriptor (stdout)
+   mov	eax,4		  ;system call number (sys_write)
+   int	0x80		  ;call kernel
 
+   mov	eax,1		  ;system call number (sys_exit)
+   int	0x80		  ;call kernel
+
+section .data
+choice DB 'y'
